f11d0568ab592a5854110b8e6120da413addd424
Ken St. Clair <kenst.clair@thismoment.com>
2012-06-12 20:30:08 -0400

    ugh

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,5 +1,5 @@
 <?php
-define('BASE_PATH', '/var/www/1stdibs_shipping/');
+define('BASE_PATH', '/Applications/XAMPP/xamppfiles/htdocs/tm/');
 
 require_once ('autoload.php');
 BENCHMARK::activate(); // activate and deactivate because you can't call the class before you include this file, but we still want an early page benchmark. 
@@ -10,5 +10,5 @@ FPX::activate();
 ERROR::activate();
 //DEBUG::activate();
 require_once(BASE_PATH . 'lib/util/common.php');
-require_once(BASE_PATH . "config/db.php");
-require_once(BASE_PATH . "config/config.auth.php");
+//require_once(BASE_PATH . "config/db.php");
+//require_once(BASE_PATH . "config/config.auth.php");
