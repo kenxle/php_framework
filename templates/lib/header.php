0580b420f8ebadf35ffabddf8a5443ee918322a5
Ken St. Clair <kenstclair@gmail.com>
2013-04-05 16:21:48 -0400

    removing specifics of a certain project.

--- templates/lib/header.php
+++ templates/lib/header.php
@@ -1,7 +1,1 @@
 <?php ?>
-
-<div id="user_info_container">
-<?if($logged_in_user){?>
-	Welcome <?=$logged_in_user->username?>. You are logged in. [ <a href="?logout=true">Logout</a> ]
-<?}?>
-</div>
